714b9104d5c754da84953aeda19b380861ae2116
Zol-By <59205376+Zol-by@users.noreply.github.com>
2023-08-07 15:28:41 +0100

    dépôt de fichier index

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <title>Mini Application PHP</title>
+</head>
+
+<body>
+    <section class="row shadow">
+        
+        <nav class="navbar bg-danger border-bottom border-body" data-bs-theme="danger">
+            <ul class="nav text-centre">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">ACCUEIL</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">APPRENANTS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">TITUTAIRS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </nav>
+    </section>
+
+    <section>
+            <h2 class="text-center">BIENVENUE DANS GESTION DES APPRENANTS</h2>
+    </section>
+
+    <section>
+
+    </section>
+</body>
+
+</html>
