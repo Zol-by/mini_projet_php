cd1801df38c686169008b355afaf035065a2715a
Mohamed <136338010+Icnane@users.noreply.github.com>
2023-08-10 14:16:03 +0100

    dépôt de fichier formulaire tuteur par Mohamed

--- formulaire_tuteur.php
+++ formulaire_tuteur.php
@@ -90,8 +90,28 @@
                         </div></br>
 
                         <div class="col-lg-12">
-                            <button class="btn btn-danger" name="btn_save" type="submit"><strong>ENREGISTRER</strong></button>
-                        </div>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal"><strong>ENREGISTRER</strong></button>
+                            </div>
+
+                            <!-- Modal"  -->
+
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel"><strong>CONFIRMATION D'ENREGISTREMENT</strong></h1>
+                                            <button type="button" class="btn-close " data-bs-dismiss="modal" aria-label="Close "></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Voulez-vous enregistrer cette personne sur la liste des tuteurs ?</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fermer</button>
+                                            <button name="btn_save" type="submit" class="btn btn-primary">Valider</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                     </form>
                 </div>
             </div>
